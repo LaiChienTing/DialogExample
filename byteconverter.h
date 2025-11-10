5b7475f401e3deda7a73f08bf7703398e73e63fb
41143158 <41143158@nfu.edu.tw>
2025-11-10 15:49:13 +0800

    Add files via upload

---
+++ byteconverter.h
@@ -0,0 +1,22 @@
+#ifndef BYTECONVERTER_H
+#define BYTECONVERTER_H
+
+#include <QDialog>
+class QLineEdit;
+class ByteConverter : public QDialog
+{
+    Q_OBJECT
+
+public:
+    ByteConverter(QWidget *parent = nullptr);
+    ~ByteConverter();
+private:
+    QLineEdit* decEdit;
+    QLineEdit* hexEdit;
+    QLineEdit* binEdit;
+private slots:
+    void decChanged(const QString&);
+    void hexChanged(const QString&);
+    void binChanged(const QString&);
+};
+#endif // BYTECONVERTER_H
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "byteconverter.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    ByteConverter w;
+    w.show();
+    return a.exec();
+}
